8869feb097292cbc376b67aecff208eb5c0ba27c
Jaylyn Stoesz <jaylynstoesz@Jaylyns-MacBook-Pro-2.local>
2018-09-14 13:01:04 -0600

    unit test

--- src/test/scala/main/AlgorithmSpec.scala
+++ src/test/scala/main/AlgorithmSpec.scala
@@ -9,15 +9,7 @@ import org.scalatest.junit.JUnitRunner
 class AlgorithmSpec extends FunSuite {
   val pattern: String = ""
 
-  test("encrypts non-empty input as MD5 hash") {
-    assert(AlgorithmA.run[String]("a").matches("a"))
-  }
-
-  test("encrypts empty input as MD5 hash") {
-    assert(AlgorithmA.run[String]("b").matches("b"))
-  }
-
-  test("encrypts non-empty input with special characters as MD5 hash") {
+  test("runs algorithm a") {
     assert(AlgorithmA.run[String]("c").matches("c"))
   }
 }
